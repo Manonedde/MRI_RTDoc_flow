eb01cd697c09ebacf42b88ec0bc22bbf61bdbee1
Manonedde <eddemanon@gmail.com>
2023-10-25 15:17:53 -0400

    add func and update script

---
+++ plots/func.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from scipy.stats import linregress
+
+def get_regression_line_stats(x, y, all_result=False, hypothesis='two-sided'):
+    """
+    Function to compute the linear least-squares regression for two sets
+    of measurements x and y.
+
+    x:              Array for x axis.
+    y:              Array for y axis.
+    all_result:     If True return an object with all regression details.
+                    
+    Return          a line containing the Pearson correlation value and
+                    the p-value, which can be added as a legend in the
+                    correlation plot.
+    """
+    result = linregress(x, y, alternative=hypothesis)
+    r, p = result[2], result[3]
+    display_line = f'Pearson coefficient: r={r:.2f}, p={p:.2f}'
+    if all_result:
+        return result
+    else:
+        return display_line
+    
--- rdt_generate_correlation_plot.py
+++ rdt_generate_correlation_plot.py
@@ -15,14 +15,13 @@ from scilpy.io.utils import add_overwrite_arg, assert_inputs_exist
 from plot_fixed_func import scatter_with_regression_line
 
 
-
 def _build_arg_parser():
     p = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
                                 description=__doc__)
     p.add_argument('in_csv',
                    help='CSV diffusion data (.csv).')
 
-    p.add_argument('--out_suffix',
+    p.add_argument('--out_suffix', default='_Pearson_Correlation.png',
                    help='Filename prefix to save figures.')
     p.add_argument('--out_dir',
                    help='Output directory to save figures. ')
@@ -34,7 +33,7 @@ def _build_arg_parser():
                            help='Color of marker. ')
     plot_opts.add_argument('--marker_edgecolor', default='#0066cc',
                            help='Color of marker edge. ')
-    plot_opts.add_argument('--regression_line_color',default='#ff0000',
+    plot_opts.add_argument('--regression_line_color', default='#ff0000',
                            help='Color of regression line. \n'
                                 'By default YlGnBu is used.')
 
@@ -52,9 +51,6 @@ def main():
     if args.out_dir is None:
         args.out_dir = './'
 
-    if args.out_suffix is None:
-        args.out_suffix = '_Pearson_Correlation.png'
-
     # Load Data frame
     df = pd.read_csv(args.in_csv)
 
@@ -62,30 +58,32 @@ def main():
     bundle_list_title = bundle_list.replace('_', ' ', regex=True)
 
     for idx, bundle in enumerate(bundle_list_title):
-        bdl = df[df.Bundles == bundle]
-        bdl=bdl.pivot(index='Sid',columns='Measures', values='Value')
-        metric_list=bdl.columns.tolist()
+        curr_bundle_df = df[df.Bundles == bundle]
+        curr_bundle_df = curr_bundle_df.pivot(
+                            index='Sid', columns='Measures', values='Value')
+        metric_list = curr_bundle_df.columns.tolist()
 
-        for i in metric_list:
-            for j in metric_list:
-                if i != j:
-                    x=bdl[i]
-                    y=bdl[j]
+        for x_axis_name in metric_list:
+            for y_axis_name in metric_list:
+                if x_axis_name != y_axis_name:
+                    x = curr_bundle_df[x_ax_axis_namexis]
+                    y = curr_bundle_df[y_axis_name]
                     # pearson correlation
-                    slope, intercept, r, p, stderr = scipy.stats.linregress(x, y)
-                    line = f'Pearson coefficient: r={r:.2f}, p={p:.2f}'
-                    figtitle = bundle + ' - Correlation between '+ i + ' and ' + j
+                    line = get_regression_line_stats(x, y)
+                    figtitle = bundle + ' - Correlation between ' +
+                               x_axis_name + ' and ' + y_axis_name
 
-                    scatter_with_regression_line(x,y, intercept + slope * x,
-                                                 xlabel=i, ylabel=j,
-                                                 marker = args.marker, 
-                                                 marker_color=args.markercolor,
-                                                 marker_edgecolors=args.marker_edgecolor,
-                                                 line_label=line,
-                                                 line_color=args.regression_line_color,
-                                                 figtitle=figtitle)
+                    scatter_with_regression_line(
+                         x, y, intercept + slope * x, xlabel=x_axis_name,
+                         ylabel=y_axis_name, marker=args.marker,
+                         marker_color=args.markercolor, line_label=line,
+                         marker_edgecolors=args.marker_edgecolor,
+                         line_color=args.regression_line_color,
+                         figtitle=figtitle)
+                    
                     curr_foler = bundle_list[idx]
-                    outname = bundle_list[idx] + '_' + i + '_' + j + args.out_suffix
+                    outname = bundle_list[idx] + '_' + \
+                        i + '_' + j + args.out_suffix
                     plt.savefig(os.path.join(args.out_dir, curr_foler, outname),
                                 dpi=500, bbox_inches='tight')
                     plt.close('all')
